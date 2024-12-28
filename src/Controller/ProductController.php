2f387b2b898a7a9dc11858d6b1b2ade9f742d941
Paul Novack <paul.novack@gmail.com>
2024-12-28 10:51:21 -0600

    fix: Remove duplicate product fetch in ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -21,7 +21,6 @@ class ProductController extends AbstractController
 
         $repository->aFindAll();
         $products = $repository->aSyncFetch();
-        $products = $repository->aSyncFetch();
 
         return $this->render('product/list.html.twig', [
             'products' => $products
