ae521a59723b6528e6f803311ce22a2fb020806b
Paul Novack <paul.novack@gmail.com>
2024-12-27 19:52:53 -0600

    refactor: Remove redundant getQuery() call in UserRepository search method

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -11,7 +11,6 @@ class UserRepository extends CustomRepository
         $queryBuilder = $this->createQueryBuilder('u')
             ->where('u.firstName LIKE :searchTerm OR u.lastName LIKE :searchTerm')
             ->setParameter('searchTerm', '%' . $searchTerm . '%')
-            ->getQuery()
             ->getQuery();
 
         $this->storeSqlForQuery($queryBuilder);
